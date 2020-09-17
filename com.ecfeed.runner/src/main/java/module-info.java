684feac8724794cf9151968d86d4cc5bdce73ce7
krzysztof skorupski <k.skorupski@testify.no>
2020-09-17 15:09:07 +0200

    update

--- com.ecfeed.runner/src/main/java/module-info.java
+++ com.ecfeed.runner/src/main/java/module-info.java
@@ -3,4 +3,5 @@ module com.ecfeed.runner {
     requires org.apache.httpcomponents.httpclient;
     requires org.json;
     exports com.ecfeed.runner;
+    exports com.ecfeed.runner.design;
 }
--- com.ecfeed.test/src/main/java/com/ecfeed/test/Main.java
+++ com.ecfeed.test/src/main/java/com/ecfeed/test/Main.java
@@ -1,10 +1,11 @@
 package com.ecfeed.test;
 
 import com.ecfeed.runner.EcFeedFactory;
+import com.ecfeed.runner.design.TestProvider;
 
 public class Main {
     public static void main(String[] args) {
-        EcFeedFactory.getTestProvider("dupa").validateConnection();
+        TestProvider provider = EcFeedFactory.getTestProvider("dupa");
 
     }
 }
